b9020242d3488739dc1e81cc59142d07ca301984
Rhyan Willian Mezaroba <428214@unoesc.lan>
2024-08-14 22:06:51 -0300

    Primeiro projeto

---
+++ Atividades/Aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿//comentário de uma linha
+/*
+    Comentário de 
+    Multiplas linhas
+*/
+
+//Declarando variáveis
+int number;
+//Atribuindo valor
+number = 10;
+
+//Declarando constante
+const ints DAYS_IN_WEEK = 7;
+
+
+//imprimindo String concatenada
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
