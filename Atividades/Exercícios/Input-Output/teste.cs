e3cc33e3b2d4330bdf71ab3752e8c28901cc134c
unknown <RhyaMezaroba>
2024-08-24 15:57:54 -0300

    Exercise  Input and Output - Pages 22 to 27

---
+++ Atividades/Exercícios/Input-Output/teste.cs
@@ -0,0 +1,7 @@
+ConsoleKeyInfo key = Console.ReadKey();
+switch (key.Key)
+{
+case ConsoleKey.S: /* Pressed S */ break;
+case ConsoleKey.F1: /* Pressed F1 */ break;
+case ConsoleKey.Escape: /* Pressed Escape */ break;
+}
