065535b0eeeb9a851238d23f7daa5c7f075d9c33
unknown <RhyaMezaroba>
2024-08-31 14:46:38 -0300

    Exercício de Repetição Impar-Par

--- Atividades/Exercícios/AtividadeImparPar/Repeticao.cs
+++ Atividades/Exercícios/AtividadeImparPar/Repeticao.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+class Program
+{    
+    static void Main()
+    {
+        ContarNumeros();
+    }
+
+    static void ContarNumeros()
+    {
+        // Variáveis para armazenar números pares e ímpares
+        Console.WriteLine("Números Pares:");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0) // Verifica se o número é par
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+        Console.WriteLine("Números Ímpares:");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 != 0) // Verifica se o número é ímpar
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
