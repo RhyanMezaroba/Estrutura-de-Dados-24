552637eb83b8e19fd98d850c21e85dd9b4a99013
Rhyan Willian Mezaroba <428214@unoesc.lan>
2024-09-04 20:23:12 -0300

    MultiDimensionalArray

---
+++ Atividades/Exercícios/MultiDimensionalArray/Program.cs
@@ -0,0 +1,27 @@
+﻿int [,] number = new int[5,2];
+
+int[,] iniNumbers = new int[,]
+{
+    {9,5,0,},
+    {3,2,-100,},
+    {43,4,-99,},
+    {95,18,-30,},
+    {8,22,-1,},
+};
+int MaiorValor = iniNumbers[0,0];
+int LinhaMaior = 0;
+int ColunaMaior = 0;
+
+    for (int i = 0; i < iniNumbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < iniNumbers.GetLength(1); j++)
+        {
+            if (iniNumbers[i,j] > MaiorValor)
+            { 
+                MaiorValor = iniNumbers[i, j];
+                LinhaMaior = i;
+                ColunaMaior = j;
+            }
+        }
+    }
+    Console.WriteLine($"O maior valor na matriz é {MaiorValor} e sua linha é {LinhaMaior} e coluna {ColunaMaior} ");
---
+++ Atividades/MultiDimensionalArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Criando um vetor multidimensional com 5 linhas e 2 colunas!
+int[,] numbers = new int[5,2];
+
+// Criando um vetor tridimensional
+int [,,] triNumbers = new int[5,4,3];
+
+//Podemos inicializar os valores do vetor no momento da sua declaração
+int[,] iniNumbers = new int[,]
+{
+    {9,5,0,}, // linha declarada abaixo
+    {93,2,-100,},
+    {43,4,-99,},
+    {95,18,-30,},
+    {8,22,-1,},
+};
+
+//Para acessar os valores contidos neste tipo de variável
+int valor = iniNumbers[0,1]; // neste caso = 5
+
+Console.WriteLine($"iniNumbers[0,1] = {valor}");
